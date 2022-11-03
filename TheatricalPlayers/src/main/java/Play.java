d72ac91f2539b6a46ddc32ea9d18babdeea535f3
Why-Not-There <Diego.FERNANDEZ_ABASCAL@etu.uca.fr>
2022-11-03 23:30:49 +0100

    Create static variables and control over play types

--- TheatricalPlayers/src/main/java/Play.java
+++ TheatricalPlayers/src/main/java/Play.java
@@ -3,8 +3,25 @@ public class Play {
   public String name;
   public String type;
 
+  
   public Play(String name, String type) {
     this.name = name;
-    this.type = type;
+    this.type = isValidType(type);
+  }
+
+  // Create static variables for differents play types and add control over play types at object creation, etc.
+
+  public static final String TRAGEDY = "tragedy";
+  public static final String COMEDY = "comedy";
+
+  public static final String[] PLAY_TYPES = {TRAGEDY, COMEDY};
+  
+  public static String isValidType(String type) {
+    for (String playType : PLAY_TYPES) {
+      if (playType.equals(type)) {
+        return type;
+      }
+    }
+    return "Unknown type";
   }
 }
