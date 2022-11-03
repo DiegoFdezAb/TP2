cacc5452099fa0d4dae2b6bf040a90e150650249
Why-Not-There <Diego.FERNANDEZ_ABASCAL@etu.uca.fr>
2022-11-03 23:45:58 +0100

    Added new test with 2 no existant types

--- TheatricalPlayers/src/test/java/StatementPrinterTests.java
+++ TheatricalPlayers/src/test/java/StatementPrinterTests.java
@@ -22,12 +22,27 @@ public class StatementPrinterTests {
 
         StatementPrinter statementPrinter = new StatementPrinter();
         var result = statementPrinter.print(invoice, plays);
-
+        System.out.println(result);
         verify(result);
     }
 
     @Test
     void statementWithNewPlayTypes() {
+        Map<String, Play> plays = Map.of(
+                "henry-v",  new Play("Henry V", "history"),
+                "as-like", new Play("As You Like It", "pastoral"));
+
+        Invoice invoice = new Invoice("BigCo", List.of(
+                new Performance("henry-v", 53),
+                new Performance("as-like", 55)));
+
+        StatementPrinter statementPrinter = new StatementPrinter();
+        Assertions.assertThrows(Error.class, () -> {
+            statementPrinter.print(invoice, plays);
+        });
+    }
+    @Test
+    void statementWithNewPlayTypes2() {
         Map<String, Play> plays = Map.of(
                 "henry-v",  new Play("Henry V", "history"),
                 "as-like", new Play("As You Like It", "pastoral"));
